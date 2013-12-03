7f9b1da09e1fd5b7885c451eaa2da5ad43c378eb
sashkavalent <sashkavalent@gmail.com>
2013-12-03 23:15:20 +0300

    get and parse html

--- popup.js
+++ popup.js
@@ -1,25 +1,42 @@
-function httpGet(theUrl)
+document.addEventListener('DOMContentLoaded', function () {
+  httpGet();
+});
+
+function setPopupHtml(host)
+{
+    anchors = document.getElementsByTagName('a');
+    for (var i = 0; i < anchors.length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            link = anchors[i].getAttribute('href');
+            anchors[i].setAttribute('href', host + link);
+        }
+        else
+        {
+            anchors[i].innerHTML = '';
+        }
+    }
+    document.head.innerHTML = '<base target="_blank">';
+}
+
+function httpGet()
 {
     var xmlHttp = null;
+    var host = 'http://localhost:3000';
 
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.open( "GET", host + '/announcements', false );
     xmlHttp.send( null );
     el = document.createElement('p');
     el.innerHTML = xmlHttp.responseText;
+
     details = new Object();
-    details.text = "123";
+    details.text = el.querySelector('.comments-header').innerHTML.replace(/\D*/g, '');
     chrome.browserAction.setBadgeText(details);
-    document.write(el.querySelector('.user a').innerText);
+
+    document.write(el.querySelector('.models').innerHTML);
+
+    setPopupHtml(host);
 }
 
-function clickHandler(e) {
-  httpGet('http://localhost:3000/ads/33')
-}
-
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers.
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', clickHandler);
-});
